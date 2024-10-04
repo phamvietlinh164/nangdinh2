f6f03fee5f13608d1af47ee24d73fbac5b244fa7
Pham Viet Linh <66984905+phamvietlinh164@users.noreply.github.com>
2024-10-04 12:39:22 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -47,7 +47,7 @@ app.use((req, res, next) => {
 
 app.get('/favicon.ico', (req, res) => res.status(204));
 // app.use('/', getData);
-app.get('/health', (req, res) => { return res.json({ message: 'ok' }) });
+app.get('/health', (req, res) => { return res.json({ message: 'ok2' }) });
 app.use('/', getDataFol2);
 // app.use('/login', login);
 // app.use('/checkauth', checkAuth);
